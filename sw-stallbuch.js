d98a69231fa260e7fc805fc50fb35be8f130d7d7
marwanahmed21999 <marwanahmed.21999@gmail.com>
2026-08-15 08:18:00 +0200

    Create sw-stallbuch.js

---
+++ sw-stallbuch.js
@@ -0,0 +1,22 @@
+const CACHE_NAME='stallbuch-pro-v1';
+const APP_SHELL=['./','./index.html'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      const fetchPromise=fetch(event.request).then(networkResponse=>{
+        caches.open(CACHE_NAME).then(cache=>cache.put(event.request,networkResponse.clone()));
+        return networkResponse;
+      }).catch(()=>cached);
+      return cached||fetchPromise;
+    })
+  );
+});
